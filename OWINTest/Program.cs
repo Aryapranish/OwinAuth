24e688bd4a9e5256a47f366bd91ac30cd80da9c0
Aryapranish <pranishstx@gmail.com>
2021-11-09 16:38:25 -0500

    Auth using OWIN

---
+++ OWINTest/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+
+namespace OWINTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:4312";
+            using (var client = new HttpClient())
+            {
+                var form = new Dictionary<string, string>
+                   {
+                       {"grant_type", "password"},
+                       {"username", "jignesh"},
+                       {"password", "user123456"},
+                   };
+                var tokenResponse = client.PostAsync(baseAddress + "/oauth/token", new FormUrlEncodedContent(form)).Result;
+                //var token = tokenResponse.Content.ReadAsStringAsync().Result;  
+                var token = tokenResponse.Content.ReadAsAsync<Token>(new[] { new JsonMediaTypeFormatter() }).Result;
+                if (string.IsNullOrEmpty(token.Error))
+                {
+                    Console.WriteLine("Token issued is: {0}", token.AccessToken);
+                }
+                else
+                {
+                    Console.WriteLine("Error : {0}", token.Error);
+                }
+                Console.Read();
+            }
+
+           
+
+
+        }
+    }
+}
---
+++ TokenBasedAuth/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+
+[assembly: OwinStartup(typeof(TokenBasedAuth.Startup))]
+namespace TokenBasedAuth
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+        }
+        private void ConfigureOAuth(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<OwinAuthDbContext>(() => new OwinAuthDbContext());
+            app.CreatePerOwinContext<UserManager<IdentityUser>>(CreateManager);
+            app.UseOAuthAuthorizationServer(new OAuthAuthorizationServerOptions
+            {
+                TokenEndpointPath = new PathString("/oauth/token"),
+                Provider = new AuthorizationServerProvider(),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(30),
+                AllowInsecureHttp = true,
+
+            });
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+        }
+
+        private static UserManager<IdentityUser> CreateManager(IdentityFactoryOptions<UserManager<IdentityUser>> options, IOwinContext context)
+        {
+            var userStore = new UserStore<IdentityUser>(context.Get<OwinAuthDbContext>());
+            var owinManager = new UserManager<IdentityUser>(userStore);
+            return owinManager;
+        }
+    }
+}
